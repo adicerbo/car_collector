64c0013b2155a8fd2a16495c739925c53dd3e6ec
adicerbo <dicerboantonio32@gmail.com>
2022-10-19 12:19:14 -0400

    create car model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    make: models.CharField(max_length=30)
+    year: models.IntegerField()
+    model: models.CharField(max_length=30)
+    trim: models.CharField(max_length=30)
+    
+
+
